e1cdea9cd1491d2e58c25d9dba531b1301784e44
Duenti <nelijfjr@gmail.com>
2015-12-30 01:10:49 -0200

    Test views and templates

---
+++ article/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.TextField(max_length=254)),
+                ('body', models.TextField()),
+                ('likes', models.IntegerField(default=0)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.TextField()),
+                ('article', models.ForeignKey(to='article.Article')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
---
+++ article/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+	title = models.TextField(max_length=254)
+	body = models.TextField()
+	likes = models.IntegerField(default=0)
+
+class Comment(models.Model):
+	article = models.ForeignKey(Article)
+	text = models.TextField()
